1ccf690c5c3371079347dbd700dfede5b13a0817
YGJH <jihuty@gmail.com>
2025-04-29 03:35:11 +0800

    ㄏ

--- FirstClass3Team.java
+++ FirstClass3Team.java
@@ -1,7 +1,11 @@
 public class FirstClass3Team {
+    public static void println(String str) {
+        System.out.println(str);
+    }
     public static void main(String[] args) {
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01257029\"\n姓名：\"蔡詠軒\"");
         System.out.println("\"學號：\"01257001\"\n姓名：\"宋辰星\"");
+        println("\"學號：\"01257049\"\n姓名：\"楊子賢\"");
     }
 }
