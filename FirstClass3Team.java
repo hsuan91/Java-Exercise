58271b22e72fae39781c1ffed8d5e206ff2b9bd9
94SungLA <90839322+94SungLA@users.noreply.github.com>
2025-04-29 02:54:23 +0800

    01257001

--- FirstClass3Team.java
+++ FirstClass3Team.java
@@ -2,5 +2,6 @@ public class FirstClass3Team {
     public static void main(String[] args) {
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01257029\"\n姓名：\"蔡詠軒\"");
+        System.out.println("\"學號：\"01257001\"\n姓名：\"宋辰星\"");
     }
 }
