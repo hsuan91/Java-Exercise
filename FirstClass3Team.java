e015db6ee19e37249790b0b3e47bed5c1c07a68d
hsuan91 <jeff20050901@gmail.com>
2025-04-28 20:48:51 +0800

    01257029

---
+++ FirstClass3Team.java
@@ -0,0 +1,6 @@
+public class FirstClass3Team {
+    public static void main(String[] args) {
+        System.out.println("這是用來練習GitHub協作的程式!");
+        System.out.println("\"學號：\"01257029\"\n姓名：\"蔡詠軒\"");
+    }
+}
